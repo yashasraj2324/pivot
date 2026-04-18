12cede0567d222895a807fa0bff45b8fd1d6f07f
kushanthkumarc <kushanth2005@gmail.com>
2026-04-18 20:54:57 +0530

    implement rigid body and loss function

--- core/__init__.py
+++ core/__init__.py
@@ -2,9 +2,13 @@
 
 from .kinematic_guardrail import (  # noqa: F401
 	COCO_BONES,
+	JOINT_ANGLE_LIMITS,
+	RIGID_REGIONS,
 	V_MAX_DEFAULT,
 	bone_length_invariance_loss,
 	compute_bone_lengths,
+	compute_rom_loss,
+	compute_rigid_topology_loss,
 	compute_velocity_loss,
 	compute_l_physics,
 )
